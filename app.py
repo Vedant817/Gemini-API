366acfd255eb6cc825203bdc6f2bf2cda8795baf
Vedant Mahajan <vedantmahajan271@gmail.com>
2023-12-23 19:04:42 +0530

    Creating a text response model using Gemini API

---
+++ app.py
@@ -0,0 +1,28 @@
+from dotenv import load_dotenv
+
+load_dotenv()  # Loading all the environment variables
+
+import streamlit as st
+import os
+import google.generativeai as genai
+
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
+# Function to load Gemini Pro Model
+model = genai.GenerativeModel('gemini-pro')
+
+
+def get_gemini_response(question):
+    response = model.generate_content(question)
+    return response.text
+
+
+st.set_page_config(page_title="Q&A Demo")
+st.header("Gemini LLM Application")
+input = st.text_input("Input: ", key="input")
+submit = st.button("Ask the Question")
+
+if submit:
+    response = get_gemini_response(input)
+    st.subheader('The response is')
+    st.write(response)
